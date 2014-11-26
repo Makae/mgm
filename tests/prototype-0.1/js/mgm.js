f154bea65c303103e092d2faa22ab13d9236c7e2
Makae <makae90@gmail.com>
2014-11-26 14:29:37 +0100

    added admin logic for markers, improoved drag and drop. round method added. and added refactoring and style improovements

--- tests/prototype-0.1/js/mgm.js
+++ tests/prototype-0.1/js/mgm.js
@@ -5,7 +5,7 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
           'map' : {
             'lat' : 51.480,
             'lng' : 0.0,
-            'zoom' : 7
+            'zoom' : 21
           },
 
           'markers' : [
@@ -48,12 +48,14 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
     this.map_dom = null;
     this.map = null;
     this.markers = [];
+    this.polygons = [];
     this.config = config;
+    this.gizmo_counter = 0;
   };
 
   MGM_Map.prototype.init = function(map_dom) {
    var mapOptions = {
-        center: mgm.latLngToPos(this.config),
+        center: mgm.utils.latLngToPos(this.config),
         zoom: this.config.zoom
       };
     this.map_dom = map_dom;
@@ -63,16 +65,59 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
 
   MGM_Map.prototype.addMarker = function(marker, builder) {
     marker.gizmo_type = 'marker';
+    marker.gizmoIdx = ++this.gizmo_counter;
+
     if(typeof builder == 'function')
       marker = builder(marker);
     else if(typeof mgm.builder.marker[marker.type] != 'undefined')
       marker = mgm.builder.marker[marker.type](marker);
     else
       marker = mgm.builder.marker.standard(marker);
+
     marker.register(this);
+    marker.idx = this.markers.length;
     this.markers.push(marker);
+
+    return marker;
   };
 
+  MGM_Map.prototype.removeMarker = function(marker) {
+    for(var i in this.markers)
+      if(this.markers[i].gizmoIdx != marker.gizmoIdx)
+        continue
+      else
+        this.markers.removed = true;
+
+    marker.unregister(this.map);
+  };
+
+  MGM_Map.prototype.addPolygon = function(polygon, builder) {
+    polygon.gizmo_type = 'polygon';
+    polygon.gizmoIdx = ++this.gizmo_counter;
+
+    if(typeof builder == 'function')
+      polygon = builder(polygon);
+    else if(typeof mgm.builder.polygon[polygon.type] != 'undefined')
+      polygon = mgm.builder.polygon[polygon.type](polygon);
+    else
+      polygon = mgm.builder.polygon.standard(polygon);
+
+    polygon.register(this);
+    polygon.idx = this.polygons.length;
+    this.polygons.push(polygon);
+
+    return polygon;
+  };
+
+  MGM_Map.prototype.removePolygon = function(polygon) {
+    for(var i in this.polygons)
+      if(this.polygons[i].gizmoIdx != polygon.gizmoIdx)
+        continue
+      else
+        this.polygons.removed = true;
+
+    polygon.unregister(this.map);
+  };
 
   var _mgm = {
     config : {
@@ -99,28 +144,29 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
       if(typeof mgm.admin != 'undefined' && mgm.admin.initialized === false)
         mgm.admin.init();
       this.initialized = true;
-    },
-    latLngToPos : function(config) {
-      return new google.maps.LatLng(config.lat, config.lng);
     }
   };
 
   mgm.builder = {
     marker: {
       standard : function(marker) {
+        var clickListenerHandler;
 
         marker.register = function(mgm_map) {
           marker.mgm_map = mgm_map;
-          marker.position = mgm.latLngToPos(marker);
+          marker.position = mgm.utils.latLngToPos(marker);
 
           marker.gm_marker = new google.maps.Marker(marker);
           marker.gm_marker.setMap(mgm_map.map);
 
-          google.maps.event.addListener(marker.gm_marker, 'click', function(e) {
-            var callback = function(data) {
-            };
-            marker.onClick(e, callback);
+          clickListenerHandler = google.maps.event.addListener(marker.gm_marker, 'click', function(e) {
+            marker.onClick(e, function(){});
           });
+        };
+
+        marker.unregister = function(mgm_map) {
+          clickListenerHandler.remove();
+          marker.gm_marker.setMap(null);
         };
 
         marker.onClick = function(e, callback) {
@@ -128,6 +174,38 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
         };
 
         return marker;
+      }
+    },
+    polygon: {
+      standard : function(polygon) {
+        var clickListenerHandler;
+
+        polygon.register = function(mgm_map) {
+          polygon.mgm_map = mgm_map;
+
+          polygon.paths = [];
+          for(var i in polygon.corners)
+            polygon.paths.push(mgm.utils.latLngToPos(polygon.corners[i]));
+
+
+          polygon.gm_polygon = new google.maps.Polygon(polygon);
+          polygon.gm_polygon.setMap(mgm_map.map);
+
+          clickListenerHandler = google.maps.event.addListener(polygon.gm_polygon, 'click', function(e) {
+            polygon.onClick(e, function(){});
+          });
+        };
+
+        polygon.unregister = function(mgm_map) {
+          clickListenerHandler.remove();
+          polygon.gm_polygon.setMap(null);
+        };
+
+        polygon.onClick = function(e, callback) {
+          mgm.content_manager.callProvider(polygon.content_provider, polygon, callback);
+        };
+
+        return polygon;
       }
     },
 
@@ -163,6 +241,13 @@ var mgm = typeof mgm != 'undefined' ? mgm : {};
       }
     }
   };
+
+  mgm.utils = {
+    latLngToPos : function(config) {
+      return new google.maps.LatLng(config.lat, config.lng);
+    }
+  };
+
   $.extend(mgm, _mgm);
 })(jQuery);
 
